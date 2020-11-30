d85ca3679015e1fc29aa7185f2717419ded9edf4
e.fedotov <fedotov1290@gmail.com>
2020-11-30 19:29:25 +0300

    build: add rollup config, dist-tests

---
+++ dist-test/browser/reflect.test.tsx
@@ -0,0 +1,117 @@
+import React, { FC, InputHTMLAttributes, ChangeEvent } from 'react';
+import { createStore, createEvent, restore } from 'effector';
+
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { reflect } from '../../browser';
+
+// Example1 (InputCustom)
+const InputCustom: FC<{
+  value: string | number | string[];
+  onChange(value: string): void;
+  testId: string;
+  placeholder?: string;
+}> = (props) => {
+  return (
+    <input
+      data-testid={props.testId}
+      placeholder={props.placeholder}
+      value={props.value}
+      onChange={(event) => props.onChange(event.currentTarget.value)}
+    />
+  );
+};
+
+test('InputCustom', async () => {
+  const change = createEvent<string>();
+  const $name = restore(change, '');
+
+  const Name = reflect({
+    view: InputCustom,
+    bind: { value: $name, onChange: change },
+  });
+
+  const container = render(<Name testId="name" />);
+
+  expect($name.getState()).toBe('');
+  await userEvent.type(container.getByTestId('name'), 'Bob');
+  expect($name.getState()).toBe('Bob');
+
+  const inputName = container.container.firstChild as HTMLInputElement;
+  expect(inputName.value).toBe('Bob');
+});
+
+test('InputCustom [replace value]', async () => {
+  const change = createEvent<string>();
+  const $name = createStore<string>('');
+
+  $name.on(change, (_, next) => next);
+
+  const Name = reflect({
+    view: InputCustom,
+    bind: { name: $name, onChange: change },
+  });
+
+  const container = render(<Name testId="name" value="Alise" />);
+
+  expect($name.getState()).toBe('');
+  await userEvent.type(container.getByTestId('name'), 'Bob');
+  expect($name.getState()).toBe('Aliseb');
+
+  const inputName = container.container.firstChild as HTMLInputElement;
+  expect(inputName.value).toBe('Alise');
+});
+
+// Example 2 (InputBase)
+const InputBase: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
+  return <input {...props} />;
+};
+
+test('InputBase', async () => {
+  const changeName = createEvent<string>();
+  const $name = restore(changeName, '');
+
+  const inputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    return event.currentTarget.value;
+  };
+
+  const Name = reflect({
+    view: InputBase,
+    bind: {
+      value: $name,
+      onChange: changeName.prepend(inputChanged),
+    },
+  });
+
+  const changeAge = createEvent<number>();
+  const $age = restore(changeAge, 0);
+  const Age = reflect({
+    view: InputBase,
+    bind: {
+      value: $age,
+      onChange: changeAge.prepend(parseInt).prepend(inputChanged),
+    },
+  });
+
+  const container = render(
+    <>
+      <Name data-testid="name" />
+      <Age data-testid="age" />
+    </>,
+  );
+
+  expect($name.getState()).toBe('');
+  await userEvent.type(container.getByTestId('name'), 'Bob');
+  expect($name.getState()).toBe('Bob');
+
+  expect($age.getState()).toBe(0);
+  await userEvent.type(container.getByTestId('age'), '25');
+  expect($age.getState()).toBe(25);
+
+  const inputName = container.getByTestId('name') as HTMLInputElement;
+  expect(inputName.value).toBe('Bob');
+
+  const inputAge = container.getByTestId('age') as HTMLInputElement;
+  expect(inputAge.value).toBe('25');
+});
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import typescript from 'rollup-plugin-typescript2';
+import multiInput from 'rollup-plugin-multi-input';
+
+const { exportRoot } = require('./lib/rollup-plugin-export-root');
+
+export default {
+  input: ['./src/browser/index.ts', './src/ssr/index.ts'],
+  output: { dir: './dist', format: 'cjs' },
+  plugins: [
+    typescript({
+      tsconfigDefaults: { compilerOptions: { declaration: true } },
+    }),
+    multiInput(),
+    exportRoot(),
+  ],
+};
