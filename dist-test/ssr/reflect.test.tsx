7541bdd0445bf4997aa91dce2af83921765b6b44
e.fedotov <fedotov1290@gmail.com>
2020-12-16 13:25:16 +0300

    test(dist-test): add 'use only event for bind'

--- dist-test/ssr/reflect.test.tsx
+++ dist-test/ssr/reflect.test.tsx
@@ -171,10 +171,97 @@ test('with ssr for client', async () => {
     </Provider>,
   );
 
-  expect($name.getState()).toBe('');
+  expect(scope.getState($name)).toBe('');
   await userEvent.type(container.getByTestId('name'), 'Bob');
   expect(scope.getState($name)).toBe('Bob');
 
   const inputName = container.getByTestId('name') as HTMLInputElement;
   expect(inputName.value).toBe('Bob');
+});
+
+test('two scopes', async () => {
+  const app = createDomain();
+
+  const changeName = app.createEvent<string>();
+  const $name = restore(changeName, '');
+
+  const Name = reflect({
+    view: InputCustom,
+    bind: { value: $name, onChange: changeName },
+  });
+
+  const scope1 = fork(app);
+  const scope2 = fork(app);
+
+  expect(scope2.getState($name)).toBe('');
+  await allSettled(changeName, { scope: scope2, params: 'Alise' });
+  expect(scope2.getState($name)).toBe('Alise');
+
+  expect(scope1.getState($name)).toBe('');
+  await allSettled(changeName, { scope: scope1, params: 'Bob' });
+  expect(scope1.getState($name)).toBe('Bob');
+
+  const container2 = render(
+    <Provider value={scope2}>
+      <Name testId="name2" />
+    </Provider>,
+  );
+  const container1 = render(
+    <Provider value={scope1}>
+      <Name testId="name1" />
+    </Provider>,
+  );
+
+  const inputName1 = container1.getByTestId('name1') as HTMLInputElement;
+  const inputName2 = container2.getByTestId('name2') as HTMLInputElement;
+
+  await allSettled(changeName, { scope: scope2, params: 'Alise' });
+  await allSettled(changeName, { scope: scope1, params: 'Bob' });
+
+  expect(scope1.getState($name)).toBe('Bob');
+  expect(scope2.getState($name)).toBe('Alise');
+
+  expect(inputName1.value).toBe('Bob');
+  expect(inputName2.value).toBe('Alise');
+});
+
+test('use only event for bind', async () => {
+  const app = createDomain();
+
+  const changeName = app.createEvent<string>();
+  const $name = restore(changeName, '');
+
+  const changeAge = app.createEvent<number>();
+
+  const Name = reflect({
+    view: InputBase,
+    bind: {
+      value: $name,
+      onChange: changeName.prepend((event) => event.currentTarget.value),
+    },
+  });
+
+  const Age = reflect({
+    view: InputBase,
+    bind: {
+      onChange: changeAge.prepend((event) =>
+        Number.parseInt(event.currentTarget.value, 10),
+      ),
+    },
+  });
+
+  const scope = fork(app);
+
+  const container = render(
+    <Provider value={scope}>
+      <Name data-testid="name" />
+      <Age data-testid="age" />
+    </Provider>,
+  );
+
+  const name = container.getByTestId('name') as HTMLInputElement;
+  const age = container.getByTestId('age') as HTMLInputElement;
+
+  expect(name.value).toBe('');
+  expect(age.value).toBe('');
 });
