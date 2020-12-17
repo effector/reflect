a68c2d0b7791eb5496e28dfa83083aa86a83bd23
e.fedotov <fedotov1290@gmail.com>
2020-12-17 16:49:35 +0300

    build: with babel

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,10 @@
 
 import typescript from 'rollup-plugin-typescript2';
 import multiInput from 'rollup-plugin-multi-input';
+import babel from 'rollup-plugin-babel';
 
 const { exportRoot } = require('./lib/rollup-plugin-export-root');
+const babelConfig = require('./babel.config.json');
 
 export default {
   input: ['./src/no-ssr/index.ts', './src/ssr/index.ts'],
@@ -12,6 +14,12 @@ export default {
   plugins: [
     typescript({
       tsconfigDefaults: { compilerOptions: { declaration: true } },
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      runtimeHelpers: true,
+      ...babelConfig,
     }),
     multiInput(),
     exportRoot(),
