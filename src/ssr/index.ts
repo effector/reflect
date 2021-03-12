2ce84aa48a43a90afd1e62eec87dec1b643c8f1f
AlexandrHoroshih <xad71rus@gmail.com>
2021-03-12 18:15:22 +0300

    feat: added `reflectList`
    Like `reflect`, but for lists. Uses effector's `useList` under the hood

--- src/ssr/index.ts
+++ src/ssr/index.ts
@@ -1,7 +1,9 @@
 import * as context from 'effector-react/ssr';
-import { variantFactory, reflectCreateFactory, reflectFactory } from '../core';
+import { variantFactory, reflectCreateFactory, reflectFactory, reflectListFactory } from '../core';
 
 export const reflect = reflectFactory(context);
 export const createReflect = reflectCreateFactory(context);
 
 export const variant = variantFactory(context);
+
+export const reflectList = reflectListFactory(context);
