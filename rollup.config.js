fb84033bfd25376ac781552598950136605d8c3b
AlexandrHoroshih <xad71rus@gmail.com>
2021-11-03 23:29:39 +0300

    feat(build): add esm build
    refactored rollup config, added esm and cjs builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,25 +3,60 @@
 import typescript from 'rollup-plugin-typescript2';
 import multiInput from 'rollup-plugin-multi-input';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
-const { exportRoot } = require('./lib/rollup-plugin-export-root');
 const babelConfig = require('./babel.config.json');
 
-export default {
-  input: ['./src/no-ssr/index.ts', './src/ssr/index.ts'],
-  output: { dir: './dist', format: 'cjs' },
-  watch: false,
-  plugins: [
-    typescript({
-      tsconfigDefaults: { compilerOptions: { declaration: true } },
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      runtimeHelpers: true,
-      ...babelConfig,
-    }),
-    multiInput(),
-    exportRoot(),
-  ],
-};
+const plugins = [
+  typescript({
+    tsconfigDefaults: { compilerOptions: { declaration: true } },
+  }),
+  babel({
+    exclude: 'node_modules/**',
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    runtimeHelpers: true,
+    ...babelConfig,
+  }),
+  nodeResolve({
+    jsnext: true,
+    skip: ['effector'],
+    extensions: ['.js', '.mjs'],
+  }),
+  commonjs({ extensions: ['.js', '.mjs'] }),
+  multiInput(),
+  terser(),
+];
+
+const noSsr = './src/index.ts';
+const ssr = './src/ssr/index.ts';
+const input = [noSsr, ssr];
+const external = ['effector', 'effector-react', 'react', 'effector-react/ssr'];
+
+export default [
+  {
+    input,
+    external,
+    plugins,
+    output: {
+      dir: './dist/esm',
+      format: 'es',
+      sourcemap: true,
+      externalLiveBindings: false,
+    },
+  },
+  {
+    input,
+    external,
+    plugins,
+    output: {
+      dir: './dist/cjs',
+      format: 'cjs',
+      freeze: false,
+      exports: 'named',
+      sourcemap: true,
+      externalLiveBindings: false,
+    },
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { reflect, createReflect, list, variant } from './no-ssr';
