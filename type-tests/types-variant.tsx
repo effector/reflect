79c5bae4703e65f36f4f5be389d9925e795f0924
Alexander Khoroshikh <32790736+AlexandrHoroshih@users.noreply.github.com>
2021-12-26 20:13:34 +0300

    feat: more tests for variant

---
+++ type-tests/types-variant.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React from 'react';
+import { expectType } from 'tsd';
+import { createStore, createEvent } from 'effector';
+import { variant } from '../src';
+
+// basic variant usage
+{
+  const Input: React.FC<{
+    value: string;
+    onChange: (newValue: string) => void;
+  }> = () => null;
+  const DateTime: React.FC<{
+    value: string;
+    onChange: (newValue: string) => void;
+  }> = () => null;
+  const $value = createStore<string>('');
+  const changed = createEvent<string>();
+  const $type = createStore<'input' | 'datetime'>('input');
+
+  const VariableInput = variant({
+    source: $type,
+    bind: {
+      value: $value,
+      onChange: changed,
+    },
+    cases: {
+      input: Input,
+      datetime: DateTime,
+    },
+  });
+
+  expectType<React.FC>(VariableInput);
+}
+
+// variant catches incompatible props between cases
+{
+  const Input: React.FC<{
+    value: string;
+    onChange: (event: { target: { value: string } }) => void;
+  }> = () => null;
+  const DateTime: React.FC<{
+    value: string;
+    onChange: (newValue: string) => void;
+  }> = () => null;
+  const $value = createStore<string>('');
+  const changed = createEvent<string>();
+  const $type = createStore<'input' | 'datetime'>('input');
+
+  const VariableInput = variant({
+    source: $type,
+    bind: {
+      value: $value,
+      // @ts-expect-error
+      onChange: changed,
+    },
+    cases: {
+      input: Input,
+      // @ts-expect-error
+      datetime: DateTime,
+    },
+  });
+
+  expectType<React.FC>(VariableInput);
+}
+
+// variant allows to set every possble case
+// for e.g. if we want to cover only specific ones and render default for the rest
+{
+  type PageProps = {
+    context: {
+      route: string;
+    };
+  };
+
+  const HomePage: React.FC<PageProps> = () => null;
+  const FaqPage: React.FC<PageProps> = () => null;
+  const NotFoundPage: React.FC<PageProps> = () => null;
+  const $page = createStore<'home' | 'faq' | 'profile' | 'products'>('home');
+  const $pageContext = $page.map((route) => ({ route }));
+
+  const CurrentPage = variant({
+    source: $page,
+    bind: { context: $pageContext },
+    // does not work yet
+    cases: {
+      home: HomePage,
+      faq: FaqPage,
+    },
+    default: NotFoundPage,
+  });
+
+  expectType<React.FC>(CurrentPage);
+}
