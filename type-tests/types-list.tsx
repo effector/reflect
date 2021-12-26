d538bcff6857e8a71ae792fdf510e98ba6f89826
Alexander Khoroshikh <32790736+AlexandrHoroshih@users.noreply.github.com>
2021-12-26 21:04:19 +0300

    feat(list): basic tests for list

---
+++ type-tests/types-list.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React from 'react';
+import { expectType } from 'tsd';
+import { createStore, createEvent } from 'effector';
+import { list } from '../src';
+
+// basic usage of list
+{
+  const Item: React.FC<{
+    id: number;
+    value: string;
+    onChange: (update: [id: string, newValue: string]) => void;
+  }> = () => null;
+  const changed = createEvent<[id: string, newValue: string]>();
+  const $items = createStore<{ id: number; value: string }[]>([]);
+
+  const List = list({
+    source: $items,
+    view: Item,
+    bind: {
+      onChange: changed,
+    },
+    mapItem: {
+      id: (item) => item.id,
+      value: (item) => item.value,
+    },
+    getKey: (item) => item.id,
+  });
+
+  expectType<React.FC>(List);
+}
+
+// list has default option for getKey, so this should not be required
+{
+  const Item: React.FC<{
+    id: number;
+    value: string;
+    onChange: (update: [id: string, newValue: string]) => void;
+  }> = () => null;
+  const changed = createEvent<[id: string, newValue: string]>();
+  const $items = createStore<{ id: number; value: string }[]>([]);
+
+  const List = list({
+    source: $items,
+    view: Item,
+    bind: {
+      onChange: changed,
+    },
+    mapItem: {
+      id: (item) => item.id,
+      value: (item) => item.value,
+    },
+  });
+
+  expectType<React.FC>(List);
+}
+
+// list highlightes missing props for items view
+// since missing props cannot be added at react later (contrary to reflect)
+{
+  const Item: React.FC<{
+    id: number;
+    value: string;
+    onChange: (update: [id: string, newValue: string]) => void;
+  }> = () => null;
+  const $items = createStore<{ id: number; value: string }[]>([]);
+
+  const List = list({
+    source: $items,
+    view: Item,
+    bind: {},
+    // @ts-expect-error
+    mapItem: {
+      id: (item) => item.id,
+      value: (item) => item.value,
+    },
+  });
+
+  expectType<React.FC>(List);
+}
