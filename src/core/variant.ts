eb3725ae0bf2f69cb9a6b91a59d6d8791fa75375
ivan posokhin <i.posokhin@redmadrobot.com>
2023-03-19 20:04:38 +0500

    improvement: remove context dependency from variant method, unwrap variant method from factory

--- src/core/variant.ts
+++ src/core/variant.ts
@@ -1,76 +1,68 @@
 import { Store } from 'effector';
+import { useUnit } from 'effector-react';
 import React from 'react';
 
-import { reflectFactory } from './reflect';
-import {
-  AtLeastOne,
-  BindableProps,
-  Context,
-  Hooks,
-  PartialBoundProps,
-  View,
-} from './types';
+import { reflect } from './reflect';
+import { AtLeastOne, BindableProps, Hooks, PartialBoundProps, View } from './types';
 
 const Default = () => null;
 
-export function variantFactory(context: Context) {
-  const reflect = reflectFactory(context);
+const isClientSide = typeof window !== 'undefined';
 
-  return function variant<
-    Props,
-    Variant extends string,
-    Bind extends BindableProps<Props>,
-  >(
-    config:
-      | {
-          source: Store<Variant>;
-          bind?: Bind;
-          cases: AtLeastOne<Record<Variant, View<Props>>>;
-          hooks?: Hooks;
-          default?: View<Props>;
-        }
-      | {
-          if: Store<boolean>;
-          then: View<Props>;
-          else?: View<Props>;
-          hooks?: Hooks;
-          bind?: Bind;
-        },
-  ): React.FC<PartialBoundProps<Props, Bind>> {
-    let $case: Store<Variant>;
-    let cases: AtLeastOne<Record<Variant, View<Props>>>;
-    let def: View<Props>;
+export function variant<
+  Props,
+  Variant extends string,
+  Bind extends BindableProps<Props>,
+>(
+  config:
+    | {
+        source: Store<Variant>;
+        bind?: Bind;
+        cases: AtLeastOne<Record<Variant, View<Props>>>;
+        hooks?: Hooks;
+        default?: View<Props>;
+      }
+    | {
+        if: Store<boolean>;
+        then: View<Props>;
+        else?: View<Props>;
+        hooks?: Hooks;
+        bind?: Bind;
+      },
+): React.FC<PartialBoundProps<Props, Bind>> {
+  let $case: Store<Variant>;
+  let cases: AtLeastOne<Record<Variant, View<Props>>>;
+  let def: View<Props>;
 
-    // Shortcut for Store<boolean>
-    if ('if' in config) {
-      $case = config.if.map((value): Variant => (value ? 'then' : 'else') as Variant);
+  // Shortcut for Store<boolean>
+  if ('if' in config) {
+    $case = config.if.map((value): Variant => (value ? 'then' : 'else') as Variant);
 
-      cases = {
-        then: config.then,
-        else: config.else,
-      } as unknown as AtLeastOne<Record<Variant, View<Props>>>;
-      def = Default;
-    }
-    // Full form for Store<string>
-    else {
-      $case = config.source;
-      cases = config.cases;
-      def = config.default ?? Default;
-    }
+    cases = {
+      then: config.then,
+      else: config.else,
+    } as unknown as AtLeastOne<Record<Variant, View<Props>>>;
+    def = Default;
+  }
+  // Full form for Store<string>
+  else {
+    $case = config.source;
+    cases = config.cases;
+    def = config.default ?? Default;
+  }
 
-    function View(props: Props) {
-      const nameOfCase = context.useUnit($case);
-      const Component = cases[nameOfCase] ?? def;
+  function View(props: Props) {
+    const nameOfCase = useUnit($case, { forceScope: !isClientSide });
+    const Component = cases[nameOfCase] ?? def;
 
-      return React.createElement(Component as any, props as any);
-    }
+    return React.createElement(Component as any, props as any);
+  }
 
-    const bind = config.bind ?? ({} as Bind);
+  const bind = config.bind ?? ({} as Bind);
 
-    return reflect({
-      bind,
-      view: View,
-      hooks: config.hooks,
-    });
-  };
+  return reflect({
+    bind,
+    view: View,
+    hooks: config.hooks,
+  });
 }
