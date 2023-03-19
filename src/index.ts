3c1ded9b3a72da909edec116e608484804ebb7f1
ivan posokhin <i.posokhin@redmadrobot.com>
2023-03-19 20:11:03 +0500

    fix: public api

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import * as context from 'effector-react';
-
-import {
-  listFactory,
-  reflectCreateFactory,
-  reflectFactory,
-  variantFactory,
-} from './core';
-
-export const reflect = reflectFactory(context);
-export const createReflect = reflectCreateFactory(context);
-
-export const variant = variantFactory(context);
-
-export const list = listFactory(context);
+export * from './core';
--- src/scope.ts
+++ src/scope.ts
@@ -1,15 +1,5 @@
-import * as effectorReactSSR from 'effector-react/scope';
+console.info(
+  '`@effector/reflect/scope` is the same as `@effector/reflect`, so you can import from `@effector/reflect`',
+);
 
-import {
-  listFactory,
-  reflectCreateFactory,
-  reflectFactory,
-  variantFactory,
-} from './core';
-
-export const reflect = reflectFactory(effectorReactSSR);
-export const createReflect = reflectCreateFactory(effectorReactSSR);
-
-export const variant = variantFactory(effectorReactSSR);
-
-export const list = listFactory(effectorReactSSR);
+export * from './core';
