d68a07f125eab76ab470e7a46536bda631753478
ivan posokhin <i.posokhin@redmadrobot.com>
2023-03-19 20:01:18 +0500

    improvement: remove context dependency from list method, unwrap list method from factory

--- src/core/list.ts
+++ src/core/list.ts
@@ -1,8 +1,9 @@
 import { Store } from 'effector';
+import { useList } from 'effector-react';
 import React from 'react';
 
-import { reflectFactory } from './reflect';
-import { BindableProps, Context, Hooks, PartialBoundProps, View } from './types';
+import { reflect } from './reflect';
+import { BindableProps, Hooks, PartialBoundProps, View } from './types';
 
 type ReflectListConfig<Props, Item, Bind> = Item extends Props
   ? {
@@ -43,50 +44,48 @@ type ReflectListConfig<Props, Item, Bind> = Item extends Props
           };
         };
 
-export function listFactory(context: Context) {
-  const reflect = reflectFactory(context);
+const isClientSide = typeof window !== 'undefined';
 
-  return function list<
-    Item extends Record<any, any>,
-    Props,
-    Bind extends BindableProps<Props> = BindableProps<Props>,
-  >(config: ReflectListConfig<Props, Item, Bind>): React.FC {
-    const ItemView = reflect<Props, Bind>({
-      view: config.view,
-      bind: config.bind ? config.bind : ({} as Bind),
-      hooks: config.hooks,
-    });
+export function list<
+  Item extends Record<any, any>,
+  Props,
+  Bind extends BindableProps<Props> = BindableProps<Props>,
+>(config: ReflectListConfig<Props, Item, Bind>): React.FC {
+  const ItemView = reflect<Props, Bind>({
+    view: config.view,
+    bind: config.bind ? config.bind : ({} as Bind),
+    hooks: config.hooks,
+  });
 
-    const listConfig = {
-      getKey: config.getKey,
-      fn: (value: Item, index: number) => {
-        const finalProps = React.useMemo(() => {
-          const props: any = {};
+  const listConfig = {
+    getKey: config.getKey,
+    fn: (value: Item, index: number) => {
+      const finalProps = React.useMemo(() => {
+        const props: any = {};
 
-          if (config.mapItem) {
-            forIn(config.mapItem, (prop) => {
-              const fn =
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                config.mapItem![prop];
-              const propValue = fn(value, index);
+        if (config.mapItem) {
+          forIn(config.mapItem, (prop) => {
+            const fn =
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              config.mapItem![prop];
+            const propValue = fn(value, index);
 
-              props[prop] = propValue;
-            });
-          } else {
-            forIn(value, (prop) => {
-              props[prop] = value[prop];
-            });
-          }
+            props[prop] = propValue;
+          });
+        } else {
+          forIn(value, (prop) => {
+            props[prop] = value[prop];
+          });
+        }
 
-          return props;
-        }, [value, index]);
+        return props;
+      }, [value, index]);
 
-        return React.createElement(ItemView, finalProps);
-      },
-    };
-
-    return () => context.useList(config.source, listConfig);
+      return React.createElement(ItemView, finalProps);
+    },
   };
+
+  return () => useList(config.source, listConfig, { forceScope: !isClientSide });
 }
 
 function forIn<T extends Record<any, any>, R extends any>(
