5bd2666cc3a805339933fa9f1cf0397d974b3f46
ivan posokhin <i.posokhin@redmadrobot.com>
2023-03-21 23:31:12 +0500

    fix: add missing context parameter

--- src/scope.ts
+++ src/scope.ts
@@ -8,7 +8,7 @@ import {
 
 const scopeContext: Context = { forceScope: true };
 
-export const reflect = reflectFactory();
+export const reflect = reflectFactory(scopeContext);
 export const createReflect = reflectCreateFactory(scopeContext);
 export const variant = variantFactory(scopeContext);
 export const list = listFactory(scopeContext);
