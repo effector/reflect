fdc5199c64d78a3060b5bef67727e2d7b6ea46c6
AlexandrHoroshih <xad71rus@gmail.com>
2023-12-16 18:38:29 +0700

    Rewrite types for reflect method

--- public-types/reflect.d.ts
+++ public-types/reflect.d.ts
@@ -1,4 +1,24 @@
-export function reflect(any: any): any;
+import type { EventCallable, Store } from 'effector';
+import type { ComponentType, FC } from 'react';
+
+type UnbindableProps = 'key' | 'ref';
+
+export function reflect<
+  Props,
+  Bind extends {
+    [K in keyof Props]?: K extends UnbindableProps
+      ? never
+      : Props[K] | Store<Props[K]> | EventCallable<void>;
+  },
+>(config: {
+  view: ComponentType<Props>;
+  bind: Bind;
+  hooks?: {
+    mounted: EventCallable<void>;
+    unmounted: EventCallable<void>;
+  };
+}): FC<Omit<Props, keyof Bind>>;
+
 export function createReflect(any: any): any;
 export function list(any: any): any;
 export function variant(any: any): any;
